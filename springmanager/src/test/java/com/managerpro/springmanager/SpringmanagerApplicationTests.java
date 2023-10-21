1c40880331c354ffc3a98d152d6aca416a8db90b
Ahmed <30933484+Ahmed-Manai@users.noreply.github.com>
2023-10-21 08:05:35 +0000

    Pending changes exported from your codespace

---
+++ springmanager/src/test/java/com/managerpro/springmanager/SpringmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerpro.springmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
